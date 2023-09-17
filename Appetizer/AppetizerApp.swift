c45fed033735f42cca32b4ea0476182831fe5926
Boris <banofon@gmail.com>
2023-09-17 16:46:50 -0400

    Initial commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by Boris Ofon on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
